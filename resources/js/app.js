748d9ba90fde5b53b7e62297d65587e8b5902cdb
Matej Arh <matej.arh@gmail.com>
2025-03-24 09:31:32 +0100

    inertia link as component

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,7 @@ import './bootstrap';
 import '../css/app.css';
 
 import { createApp, h } from 'vue';
-import { createInertiaApp } from '@inertiajs/vue3';
+import { createInertiaApp, Link } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
@@ -15,9 +15,10 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .component('InertiaLink', Link)
             .mount(el);
     },
     progress: {
-        color: '#4B5563',
+        color: '#024383',
     },
 });
