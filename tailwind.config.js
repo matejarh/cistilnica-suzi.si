20934fa80ab7f0c867da7e334ac15968a4719edf
Matej Arh <matej.arh@gmail.com>
2025-03-24 14:10:03 +0100

    fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,43 +1,43 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
-import typography from '@tailwindcss/typography';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
+import typography from "@tailwindcss/typography";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './vendor/laravel/jetstream/**/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.vue',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./vendor/laravel/jetstream/**/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.vue",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Lato", "sans-serif"],
+                heading: ["Poppins", "sans-serif"],
             },
             colors: {
                 primary: {
-                  light: '#7dd3fc', // sky-300
-                  DEFAULT: '#0ea5e9', // sky-500
-                  dark: '#0369a1', // blue-700
+                    light: "#7dd3fc", // sky-300
+                    DEFAULT: "#0ea5e9", // sky-500
+                    dark: "#0369a1", // blue-700
                 },
                 secondary: {
-                  DEFAULT: '#14b8a6', // teal-500
-                  light: '#6ee7b7', // emerald-400
+                    DEFAULT: "#14b8a6", // teal-500
+                    light: "#6ee7b7", // emerald-400
                 },
                 neutral: {
-                  light: '#f8fafc', // slate-100
-                  DEFAULT: '#e5e7eb', // gray-300
-                  dark: '#334155', // slate-700
+                    light: "#f8fafc", // slate-100
+                    DEFAULT: "#e5e7eb", // gray-300
+                    dark: "#334155", // slate-700
                 },
                 accent: {
-                  DEFAULT: '#06b6d4', // cyan-400
-                  dark: '#0891b2', // cyan-600
+                    DEFAULT: "#06b6d4", // cyan-400
+                    dark: "#0891b2", // cyan-600
                 },
-              },
-
+            },
         },
     },
 
