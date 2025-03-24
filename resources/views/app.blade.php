08c6fb66f11a77332bb48d96b94e55357dffb45a
Matej Arh <matej.arh@gmail.com>
2025-03-24 16:42:56 +0100

    fonts

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,6 +9,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&family=Lato:wght@400;700&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
         @routes
