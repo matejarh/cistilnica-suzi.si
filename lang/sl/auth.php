2855a5709e3174597f3f9eafbaeae81333d8f26d
Matej Arh <matej.arh@gmail.com>
2025-04-03 20:33:08 +0200

    update

--- lang/sl/auth.php
+++ lang/sl/auth.php
@@ -5,5 +5,5 @@ declare(strict_types=1);
 return [
     'failed'   => 'Ti podatki se ne ujemajo z našimi.',
     'password' => 'Greslo ni pravilno.',
-    'throttle' => 'Preveč poskusov prijave. Prosimo, poskusite ponovno čez :seconds sekund.',
+    'throttle' => 'Preveč poizkusov prijave. Prosimo, poskusite ponovno čez :seconds sekund.',
 ];
