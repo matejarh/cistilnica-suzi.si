9d9a95ffd5e1b612857e42b340606ac464f17583
Matej Arh <matej.arh@gmail.com>
2025-04-05 11:33:43 +0200

    add margin-bottom to HeroSection container for improved layout

--- resources/js/Pages/Landing.vue
+++ resources/js/Pages/Landing.vue
@@ -16,8 +16,9 @@ const appName = import.meta.env.VITE_APP_NAME;
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-primary overflow-hidden shadow-xl shadow-primary sm:rounded-lg bg-opacity-60 backdrop-blur-sm">
+                <div class="bg-primary overflow-hidden shadow-xl shadow-primary sm:rounded-lg bg-opacity-60 backdrop-blur-sm mb-24">
                     <HeroSection />
+
                 </div>
             </div>
         </div>
