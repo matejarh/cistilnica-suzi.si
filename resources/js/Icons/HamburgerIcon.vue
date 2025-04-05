e4fa7ea5522802f461be1379021d90dceff5f997
Matej Arh <matej.arh@gmail.com>
2025-04-05 12:14:23 +0200

    feat: add HamburgerIcon component for navigation toggle

---
+++ resources/js/Icons/HamburgerIcon.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+    showingNavigationDropdown: Boolean,
+});
+</script>
+
+<template>
+    <svg stroke="currentColor" fill="none" viewBox="0 0 24 24">
+        <path :class="{ hidden: showingNavigationDropdown, 'inline-flex': !showingNavigationDropdown }"
+            stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+        <path :class="{ hidden: !showingNavigationDropdown, 'inline-flex': showingNavigationDropdown }"
+            stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+    </svg>
+</template>
