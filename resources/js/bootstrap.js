16b80085071995c6c1a32a9340f47e5010cb3019
Matej Arh <matej.arh@gmail.com>
2025-04-05 12:13:42 +0200

    refactor axios configuration for improved readability and organization

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,25 +1,29 @@
 import axios from 'axios';
+import Logo from '../images/logo1.png';
+
 window.axios = axios;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-window.axios.defaults.headers.common['Content-Security-Policy'] = "default-src 'self'; frame-ancestors 'none'";
-window.axios.defaults.headers.common['X-Content-Type-Options'] = 'nosniff';
-window.axios.defaults.headers.common['X-Frame-Options'] = 'DENY';
-window.axios.defaults.headers.common['X-XSS-Protection'] = '1; mode=block';
-window.axios.defaults.headers.common['Referrer-Policy'] = 'strict-origin-when-cross-origin';
-window.axios.defaults.headers.common['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains; preload';
-window.axios.defaults.headers.common['Feature-Policy'] = "geolocation 'none'; midi 'none'; notifications 'none'; push 'none'; sync-xhr 'none'; microphone 'none'; camera 'none'; magnetometer 'none'; gyroscope 'none'; speaker 'none'; vibrate 'none'; fullscreen 'none'; payment 'none'; usb 'none'";
-window.axios.defaults.headers.common['Permissions-Policy'] = "geolocation=(), microphone=(), camera=(), midi=(), encrypted-media=()";
-
-import Logo from '../images/logo1.png'
+window.axios.defaults.headers.common = {
+    'X-Requested-With': 'XMLHttpRequest',
+    'Content-Security-Policy': "default-src 'self'; frame-ancestors 'none'",
+    'X-Content-Type-Options': 'nosniff',
+    'X-Frame-Options': 'DENY',
+    'X-XSS-Protection': '1; mode=block',
+    'Referrer-Policy': 'strict-origin-when-cross-origin',
+    'Strict-Transport-Security': 'max-age=31536000; includeSubDomains; preload',
+    'Feature-Policy': "geolocation 'none'; midi 'none'; notifications 'none'; push 'none'; sync-xhr 'none'; microphone 'none'; camera 'none'; magnetometer 'none'; gyroscope 'none'; speaker 'none'; vibrate 'none'; fullscreen 'none'; payment 'none'; usb 'none'",
+    'Permissions-Policy': "geolocation=(), microphone=(), camera=(), midi=(), encrypted-media=()",
+    'Cross-Origin-Embedder-Policy': 'require-corp',
+    'Cross-Origin-Opener-Policy': 'same-origin',
+    'Cross-Origin-Resource-Policy': 'same-origin',
+};
 
 const setFavicon = () => {
-    let link = document.querySelector("link[rel*='icon']") || document.createElement('link');
-    // link.type = 'image/png';
+    const link = document.querySelector("link[rel*='icon']") || document.createElement('link');
     link.type = 'image/png';
     link.rel = 'shortcut icon';
     link.href = Logo;
-    document.getElementsByTagName('head')[0].appendChild(link);
-}
+    document.head.appendChild(link);
+};
 
-setFavicon()
+setFavicon();
