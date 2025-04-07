b8b50a1bc8f5814eccf8a47e2aaceab87dade4be
Matej Arh <matej.arh@gmail.com>
2025-04-07 20:28:23 +0200

    fix: replace Link component with inertia-link for consistency in Footer.vue

--- resources/js/Layouts/Footer.vue
+++ resources/js/Layouts/Footer.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { computed } from 'vue';
-import { Link } from '@inertiajs/vue3';
 
 const year = computed(() => new Date().getFullYear());
 
@@ -13,16 +12,16 @@ const links = [
 </script>
 
 <template>
-    <footer class="bg-primary shadow-sm mt-4 absolute bottom-0 w-full bg-opacity-60 backdrop-blur-md">
+    <footer class="bg-primary shadow-sm mt-4 absolute bottom-0 w-full bg-opacity-40 backdrop-blur-md z-10">
         <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
             <span class="text-sm text-neutral-light sm:text-center">
                 © {{ year }}
-                <Link :href="route('home')" class="hover:underline">Čistilnica Suzi</Link>.
+                <inertia-link :href="route('home')" class="hover:underline">Čistilnica Suzi</inertia-link>.
                 Vse pravice pridržane.
             </span>
             <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-neutral-light sm:mt-0">
                 <li v-for="link in links" :key="link.route" class="me-4 md:me-6">
-                    <Link :href="route(link.route)" class="hover:underline">{{ link.label }}</Link>
+                    <inertia-link :href="route(link.route)" class="hover:underline">{{ link.label }}</inertia-link>
                 </li>
             </ul>
         </div>
