438fce99c57638f6e90eac8b954df3a6d471a6ed
Matej Arh <matej.arh@gmail.com>
2025-04-07 20:00:06 +0200

    fix: comment out header section in SiteLayout for future reference

--- resources/js/Layouts/SiteLayout.vue
+++ resources/js/Layouts/SiteLayout.vue
@@ -85,11 +85,11 @@ onBeforeUnmount(() => {
             <Navigation />
 
             <!-- Page Heading -->
-            <header v-if="$slots.header" class="bg-primary text-white z-40 bg-opacity-70 backdrop-blur-md">
+            <!-- <header v-if="$slots.header" class="bg-primary text-white z-40 bg-opacity-70 backdrop-blur-md">
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 font-heading">
                     <slot name="header" />
                 </div>
-            </header>
+            </header> -->
 
             <!-- Page Content -->
             <main
