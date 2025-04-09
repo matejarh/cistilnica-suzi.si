63e5fe7bc51db16cf8e02f772d76b776ce068fd2
Matej Arh <matej.arh@gmail.com>
2025-04-09 21:05:42 +0200

    feat: add Features component with service highlights and descriptions

---
+++ resources/js/Components/Features.vue
@@ -0,0 +1,60 @@
+<script setup>
+</script>
+
+<template>
+    <section class="bg-primary-dark text-neutral-light bg-opacity-30 backdrop-blur-sm">
+        <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+            <div class="max-w-screen-md mx-auto mb-8 lg:mb-16 text-center">
+                <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-primary-light">Zakaj izbrati Čistilnico
+                    Suzi?</h2>
+                <p class="sm:text-xl">
+                    Čistilnica Suzi je družinsko podjetje z več kot 35-letno tradicijo. Skozi vsa ta leta nas vodi ista
+                    vizija – ponuditi kakovostne, zanesljive in profesionalne storitve čiščenja, ki jih naši naročniki
+                    lahko brez skrbi prepustijo v naše roke.
+                </p>
+            </div>
+            <div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
+                <!-- Feature 1 -->
+                <div class="flex flex-col items-center text-center">
+                    <div class="flex justify-center items-center mb-4 w-16 h-16 rounded-full bg-primary-light">
+                        <svg class="w-8 h-8 text-primary-dark" xmlns="http://www.w3.org/2000/svg" fill="none"
+                            viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M3 10h11M9 21V3m12 7h-6m6 4h-6m6 4h-6" />
+                        </svg>
+                    </div>
+                    <h3 class="mb-2 text-xl font-bold text-primary-light">Kvaliteta in natančnost</h3>
+                    <p>Zavezani smo kvaliteti, natančnosti in spoštovanju dogovorjenih rokov.</p>
+                </div>
+                <!-- Feature 2 -->
+                <div class="flex flex-col items-center text-center">
+                    <div class="flex justify-center items-center mb-4 w-16 h-16 rounded-full bg-primary-light">
+                        <svg class="w-8 h-8 text-primary-dark" xmlns="http://www.w3.org/2000/svg" fill="none"
+                            viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M12 8c-3.866 0-7 1.79-7 4v5h14v-5c0-2.21-3.134-4-7-4zM12 8V6m0 2a4 4 0 100-8 4 4 0 000 8z" />
+                        </svg>
+                    </div>
+                    <h3 class="mb-2 text-xl font-bold text-primary-light">Osebni pristop</h3>
+                    <p>Naši dolgoletni partnerji – tako posamezniki kot podjetja – cenijo naš osebni pristop in občutek
+                        za detajle. Vsak kos perila obravnavamo z mislijo, da si zasluži najboljšo nego.</p>
+                </div>
+                <!-- Feature 3 -->
+                <div class="flex flex-col items-center text-center">
+                    <div class="flex justify-center items-center mb-4 w-16 h-16 rounded-full bg-primary-light">
+                        <svg class="w-8 h-8 text-primary-dark" xmlns="http://www.w3.org/2000/svg" fill="none"
+                            viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M3 7h18M3 12h18M3 17h18" />
+                        </svg>
+                    </div>
+                    <h3 class="mb-2 text-xl font-bold text-primary-light">Sodobna oprema</h3>
+                    <p>S sodobno opremo in dolgoletnimi izkušnjami poskrbimo, da vaše perilo ni samo čisto, ampak
+                        tudi sveže in skrbno obdelano.</p>
+                </div>
+            </div>
+
+
+        </div>
+    </section>
+</template>
