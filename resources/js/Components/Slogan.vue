53584fd1e5074d2dad49a63a8dba0d9986ef121a
Matej Arh <matej.arh@gmail.com>
2025-04-09 21:06:18 +0200

    feat: add Slogan component with introductory text and styling

---
+++ resources/js/Components/Slogan.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+
+<template>
+
+    <section class="bg-primary text-neutral-light bg-opacity-30 backdrop-blur-sm">
+        <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+            <div class="max-w-screen-md mx-auto text-center">
+                <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-primary-light">Čistilnica
+                    Suzi</h2>
+                <p class="sm:text-xl">
+                    kjer tradicija sreča strokovnost
+                </p>
+            </div>
+        </div>
+    </section>
+</template>
