7c31a23c504dcd82be55454390c2d8abb299a9ff
Matej Arh <matej.arh@gmail.com>
2025-04-09 14:38:15 +0200

    fix: add z-index to ScrollToTop button for improved visibility

--- resources/js/Components/ScrollToTop.vue
+++ resources/js/Components/ScrollToTop.vue
@@ -20,7 +20,7 @@ const scrollToTop = () => {
       leave-from-class="opacity-100 translate-y-0 sm:scale-100"
       leave-to-class="opacity-0 translate-y-16 sm:scale-95"
     >
-      <div class="absolute bottom-1 right-6" v-show="scrollTop > 200">
+      <div class="absolute bottom-1 right-6 z-50" v-show="scrollTop > 200">
           <button
             @click="scrollToTop"
             class="inline-flex bg-opacity-20 hover:bg-opacity-70 justify-center p-0 text-primary-400 rounded cursor-pointer hover:text-primary-900 bg-primary-300  transition ease-in-out duration-300 hover:-translate-y-1"
