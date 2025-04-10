6c77cfe2be7352969865f6fbad38cdc6663a3e94
Matej Arh <matej.arh@gmail.com>
2025-04-10 15:08:33 +0200

    fix: update confirmation links to use route helper for consistency and security

--- resources/views/emails/confirm-subscription.blade.php
+++ resources/views/emails/confirm-subscription.blade.php
@@ -6,9 +6,9 @@
 
 @section('content')
     <p>Hvala, ker ste se prijavili na naše novice! Za dokončanje prijave prosimo, da potrdite svoj e-poštni naslov s klikom na spodnji gumb.</p>
-    <a href="{!! url("/prijava-na-promocije/potrditev?token={$token}&email={$email}") !!}" class="button">Potrdi naročnino</a>
+    <a href="{{ route('subscribers.confirm', ['token' => $token, 'email' => $email]) }}" class="button">Potrdi naročnino</a>
     <p>Če povezava ne deluje, kopirajte in prilepite naslednji URL v svoj brskalnik:</p>
-    <p>{!! url("/prijava-na-promocije/potrditev?token={$token}&email={$email}") !!}</p>
+    <p style="line-height: 12pt; font-size: small;"><small>{{ route('subscribers.confirm', ['token' => $token, 'email' => $email]) }}</small></p>
 @endsection
 
 @section('footer-text', 'Čistilnica Suzi - brezhibna čistoča za vaše perilo.')
