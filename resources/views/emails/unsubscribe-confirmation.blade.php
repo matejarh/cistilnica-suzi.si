b1a8fbdd98ffd983ce9b3bcf466512d0b601c3df
Matej Arh <matej.arh@gmail.com>
2025-04-10 23:38:35 +0200

    fix: update unsubscribe confirmation email template to use blade syntax for route rendering

--- resources/views/emails/unsubscribe-confirmation.blade.php
+++ resources/views/emails/unsubscribe-confirmation.blade.php
@@ -6,9 +6,9 @@
 
 @section('content')
     <p>Žal nam je, da se poslavljate! Za dokončanje odjave od naših akcij in promocij prosimo, da potrdite svojo odločitev s klikom na spodnji gumb.</p>
-    <a href="{{ route('subscribers.unsubscribe.confirm', ['token' => $token, 'email' => $email]) }}" class="button">Potrdi odjavo</a>
+    <a href="{!! route('subscribers.unsubscribe.confirm', ['token' => $token, 'email' => $email]) !!}" class="button">Potrdi odjavo</a>
     <p>Če povezava ne deluje, kopirajte in prilepite naslednji URL v svoj brskalnik:</p>
-    <p style="line-height: 12pt; font-size: small;"><small>{{ route('subscribers.unsubscribe.confirm', ['token' => $token, 'email' => $email]) }}</small></p>
+    <p style="line-height: 12pt; font-size: small;"><small>{!! route('subscribers.unsubscribe.confirm', ['token' => $token, 'email' => $email]) !!}</small></p>
 @endsection
 
 @section('footer-text', 'Čistilnica Suzi - vedno smo tu za vas, ko nas potrebujete.')
