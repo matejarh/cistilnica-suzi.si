7fe86e0d68cde818fd4e04a284c234f12cf78de2
Matej Arh <matej.arh@gmail.com>
2025-04-10 21:20:49 +0200

    feat: add icon components for success, danger, and close to enhance banner functionality

--- resources/js/Components/Banner.vue
+++ resources/js/Components/Banner.vue
@@ -1,6 +1,9 @@
 <script setup>
 import { ref, watchEffect } from 'vue';
 import { usePage } from '@inertiajs/vue3';
+import SuccessIcon from '@/Icons/SuccessIcon.vue';
+import DangerIcon from '@/Icons/DangerIcon.vue';
+import CloseIcon from '@/Icons/CloseIcon.vue';
 
 const page = usePage();
 const show = ref(true);
@@ -16,9 +19,9 @@ watchEffect(async () => {
 
 <template>
     <div>
-        <Transition enter-active-class="ease-out duration-300" enter-from-class="opacity-0 -translate-y-4  "
-            enter-to-class="opacity-100 translate-y-0 " leave-active-class="ease-in duration-200"
-            leave-from-class="opacity-100 translate-y-0 " leave-to-class="opacity-0 -translate-y-4  ">
+        <Transition enter-active-class="ease-out duration-300" enter-from-class="opacity-0 -translate-y-4"
+            enter-to-class="opacity-100 translate-y-0" leave-active-class="ease-in duration-200"
+            leave-from-class="opacity-100 translate-y-0" leave-to-class="opacity-0 -translate-y-4">
             <div v-if="show && message" :class="{ 'bg-green-700': style == 'success', 'bg-red-700': style == 'danger' }"
                 class="absolute top-0 z-[1000] w-full">
                 <div class="max-w-screen-xl mx-auto py-2 px-3 sm:px-6 lg:px-8">
@@ -26,24 +29,11 @@ watchEffect(async () => {
                         <div class="w-0 flex-1 flex items-center min-w-0">
                             <span class="flex p-2 rounded-lg"
                                 :class="{ 'bg-green-600': style == 'success', 'bg-red-600': style == 'danger' }">
-                                <svg v-if="style == 'success'" class="size-5 text-white"
-                                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                    stroke-width="1.5" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                        d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                </svg>
-
-                                <svg v-if="style == 'danger'" class="size-5 text-white"
-                                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                    stroke-width="1.5" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                        d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
-                                </svg>
+                                <SuccessIcon class="size-5 text-white" v-if="style == 'success'" />
+                                <DangerIcon class="size-5 text-white" v-if="style == 'danger'" />
                             </span>
 
-                            <p class="ms-3 font-medium text-sm text-white truncate" v-html="message">
-
-                            </p>
+                            <p class="ms-3 font-medium text-sm text-white truncate" v-html="message"></p>
                         </div>
 
                         <div class="shrink-0 sm:ms-3">
@@ -51,10 +41,7 @@ watchEffect(async () => {
                                 class="-me-1 flex p-2 rounded-md focus:outline-none sm:-me-2 transition"
                                 :class="{ 'hover:bg-green-600 focus:bg-green-600': style == 'success', 'hover:bg-red-600 focus:bg-red-600': style == 'danger' }"
                                 aria-label="Dismiss" @click.prevent="show = false">
-                                <svg class="size-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                    viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                                </svg>
+                                <CloseIcon class="size-5 text-white" />
                             </button>
                         </div>
                     </div>
