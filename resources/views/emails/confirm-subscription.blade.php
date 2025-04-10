71a1594f27eb0aa744b7030effdef9ea61a44fdb
Matej Arh <matej.arh@gmail.com>
2025-04-10 23:38:29 +0200

    fix: update confirmation link in subscription email template to use correct route

--- resources/views/emails/confirm-subscription.blade.php
+++ resources/views/emails/confirm-subscription.blade.php
@@ -6,9 +6,9 @@
 
 @section('content')
     <p>Hvala, ker ste se prijavili na naše novice! Za dokončanje prijave prosimo, da potrdite svoj e-poštni naslov s klikom na spodnji gumb.</p>
-    <a href="{{ route('subscribers.confirm', ['token' => $token, 'email' => $email]) }}" class="button">Potrdi naročnino</a>
+    <a href="{!! route('subscribers.store', ['token' => $token, 'email' => $email]) !!}" class="button">Potrdi naročnino</a>
     <p>Če povezava ne deluje, kopirajte in prilepite naslednji URL v svoj brskalnik:</p>
-    <p style="line-height: 12pt; font-size: small;"><small>{{ route('subscribers.confirm', ['token' => $token, 'email' => $email]) }}</small></p>
+    <p style="line-height: 12pt; font-size: small;"><small>{!! route('subscribers.store', ['token' => $token, 'email' => $email]) !!}</small></p>
 @endsection
 
 @section('footer-text', 'Čistilnica Suzi - brezhibna čistoča za vaše perilo.')
