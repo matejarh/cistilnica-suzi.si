0632fc41f339b22a616a7a2b42ae28deae4a6431
Matej Arh <matej.arh@gmail.com>
2025-04-10 10:27:16 +0200

    feat: define fillable attributes and casts in Subscriber model

--- app/Models/Subscriber.php
+++ app/Models/Subscriber.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Subscriber extends Model
 {
-    //
+    protected $fillable = [
+        'email',
+        'is_subscribed',
+        'ip_address',
+        'user_agent',
+    ];
+
+    protected $casts = [
+        'is_subscribed' => 'boolean',
+    ];
+
+    public function getRouteKeyName()
+    {
+        return 'email';
+    }
+
+
 }
