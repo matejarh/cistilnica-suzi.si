64db65dd1e43d51ba6e37a1603c692cf5438adb5
Matej Arh <matej.arh@gmail.com>
2025-04-11 21:50:07 +0200

    feat: implement CRUD methods for Promotion resource in PromotionController

--- app/Http/Controllers/PromotionController.php
+++ app/Http/Controllers/PromotionController.php
@@ -2,9 +2,68 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Promotion;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class PromotionController extends Controller
 {
-    //
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function index()
+    {
+        return inertia()->render('Promotions/Index', [
+            'promotions' => Promotion::latest()->paginate(10),
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Promotion  $promotion
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Promotion $promotion)
+    {
+        return inertia()->render('Promotions/Show', [
+            'promotion' => $promotion,
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Promotion  $promotion
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Promotion $promotion)
+    {
+        return inertia()->render('Promotions/Edit', [
+            'promotion' => $promotion,
+        ]);
+    }
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Promotion  $promotion
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update(Request $request, Promotion $promotion)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'start_date' => 'required|date',
+            'end_date' => 'required|date|after_or_equal:start_date',
+        ]);
+
+        $promotion->update($request->all());
+
+        return redirect()->route('promotions.index')->with('success', __('Promotion updated successfully.'));
+    }
 }
