ddd3f799170e617f1d878a8b270c219d8062fbca
Matej Arh <matej.arh@gmail.com>
2025-04-12 03:12:30 +0200

    feat: add promotion email template for new promotions

---
+++ resources/views/emails/promotion.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.email')
+
+@section('title', 'Nova promocija: ' . $promotion->name)
+
+@section('header-title', 'Nova promocija')
+
+@section('content')
+    <p>Pozdravljeni, {{ $subscriber->name }}!</p>
+    <p>Imamo novo promocijo za vas:</p>
+
+    <h2>{{ $promotion->name }}</h2>
+    <p>{{ $promotion->description }}</p>
+
+    @if ($promotion->image_url)
+        <img src="{{ $promotion->image_url }}" alt="{{ $promotion->name }}" style="max-width: 100%; height: auto;">
+    @endif
+
+    <p>Promocija velja od {{ $promotion->formatted_start_date }} do {{ $promotion->formatted_end_date }}.</p>
+    <p>Ne zamudite te priložnosti!</p>
+@endsection
+
+@section('footer-text', 'Čistilnica Suzi - brezhibna čistoča za vaše perilo.')
