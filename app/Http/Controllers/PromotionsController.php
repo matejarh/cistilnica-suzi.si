5c1f75687e35f17934e16844b52f17abf21d9d97
Matej Arh <matej.arh@gmail.com>
2025-04-12 03:10:59 +0200

    feat: enhance PromotionsController with filtering, validation, and improved response messages

--- app/Http/Controllers/PromotionsController.php
+++ app/Http/Controllers/PromotionsController.php
@@ -4,28 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Models\Promotion;
 use Illuminate\Http\Request;
-use Inertia\Inertia;
 
 class PromotionsController extends Controller
 {
-
     /**
      * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
-    public function index()
+    public function index(Request $request)
     {
         return inertia()->render('Promotions/Index', [
-            'promotions' => Promotion::latest()->paginate(10),
+            'promotions' => Promotion::filter($request->get('search'), $request->get('trashed'))
+                ->latest()
+                ->paginate(10),
+            'filters' => $request->only('search', 'trashed'),
         ]);
     }
 
     /**
      * Display the specified resource.
-     *
-     * @param  \App\Models\Promotion  $promotion
-     * @return \Illuminate\Http\Response
      */
     public function show(Promotion $promotion)
     {
@@ -36,9 +32,6 @@ class PromotionsController extends Controller
 
     /**
      * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Promotion  $promotion
-     * @return \Illuminate\Http\Response
      */
     public function edit(Promotion $promotion)
     {
@@ -46,24 +39,95 @@ class PromotionsController extends Controller
             'promotion' => $promotion,
         ]);
     }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $this->validatePromotion($request);
+
+        Promotion::create($request->all());
+
+        return $this->redirectWithMessage('promotions.index', __('Akcija uspešno ustvarjena'));
+    }
+
     /**
      * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Promotion  $promotion
-     * @return \Illuminate\Http\RedirectResponse
      */
     public function update(Request $request, Promotion $promotion)
     {
-        $request->validate([
-            'title' => 'required|string|max:255',
-            'description' => 'required|string',
-            'start_date' => 'required|date',
-            'end_date' => 'required|date|after_or_equal:start_date',
-        ]);
+        $this->validatePromotion($request, true);
 
         $promotion->update($request->all());
 
-        return redirect()->route('promotions.index')->with('success', __('Promotion updated successfully.'));
+        return $this->redirectWithMessage('promotions.index', __('Akcija uspešno posodobljena'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Promotion $promotion)
+    {
+        $promotion->delete();
+
+        return $this->redirectWithMessage('promotions.index', __('Akcija uspešno izbrisana'));
+    }
+
+    /** Send given promotion to all active subscribers
+     * @param Promotion $promotion
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function send(Promotion $promotion)
+    {
+        // Assuming you have a method to send the promotion to subscribers
+        $promotion->sendToSubscribers();
+
+        return $this->redirectWithMessage('promotions.index', __('Akcija se uspešno pošilja vsem aktivnim naročnikom'));
+    }
+
+    /**
+     * Validate promotion data.
+     */
+    private function validatePromotion(Request $request, $isUpdate = false)
+    {
+        $rules = [
+            'name' => 'required|string|max:255',
+            'description' => 'required|string',
+            'start_date' => 'required|date|before:end_date',
+            'end_date' => 'required|date|after:start_date',
+        ];
+
+        if ($isUpdate) {
+            $rules['start_date'] = 'required|date';
+            $rules['end_date'] = 'required|date|after_or_equal:start_date';
+        }
+
+        $messages = [
+            'start_date.before' => __('Začetni datum mora biti pred končnim datumom.'),
+            'start_date.required' => __('Začetni datum je obvezen.'),
+            'start_date.date' => __('Začetni datum mora biti veljaven datum.'),
+            'end_date.after' => __('Končni datum mora biti po začetnem datumu.'),
+            'end_date.required' => __('Končni datum je obvezen.'),
+            'end_date.date' => __('Končni datum mora biti veljaven datum.'),
+            'name.required' => __('Ime promocije je obvezno.'),
+            'name.string' => __('Ime promocije mora biti besedilo.'),
+            'name.max' => __('Ime promocije ne sme biti daljše od 255 znakov.'),
+            'description.required' => __('Opis promocije je obvezen.'),
+            'description.string' => __('Opis promocije mora biti besedilo.'),
+        ];
+
+        $request->validate($rules, $messages);
+    }
+
+    /**
+     * Flash a success message and redirect to a route.
+     */
+    private function redirectWithMessage($route, $message)
+    {
+        session()->flash('flash.banner', $message);
+        session()->flash('flash.bannerStyle', 'success');
+
+        return redirect()->route($route)->with('success', $message);
     }
 }
