58bbbe3db44eaad3f0d54a07505ee4ad3dd07841
Matej Arh <matej.arh@gmail.com>
2025-04-15 13:52:18 +0200

    feat: add ConfirmSubscription Mailable for handling subscription confirmation emails

---
+++ app/Mail/ConfirmSubscription.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class ConfirmSubscription extends Mailable implements ShouldQueue
+{
+    use Queueable, SerializesModels;
+
+    public $token;
+    public $email;
+    public $link;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(string $token, string $email, string $link)
+    {
+        $this->link = $link;
+        $this->token = $token;
+        $this->email = $email;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Potrdi prijavo na akcije',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.confirm-subscription',
+            with: [
+                'token' => $this->token,
+                'email' => $this->email,
+                'link' => $this->link,
+            ],
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
