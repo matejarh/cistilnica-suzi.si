7be80bd6ca24d4b3fa20af6220a8f665cd7fddaf
Matej Arh <matej.arh@gmail.com>
2025-04-15 23:06:16 +0200

    feat: wrap action buttons in Tooltip components for better user guidance

--- resources/js/Components/PromotionCard.vue
+++ resources/js/Components/PromotionCard.vue
@@ -2,6 +2,7 @@
 import PaperAirplaneIcon from '@/Icons/PaperAirplaneIcon.vue';
 import PencileSquareIcon from '@/Icons/PencileSquareIcon.vue';
 import TrashIcon from '@/Icons/TrashIcon.vue';
+import Tooltip from './Tooltip.vue';
 
 defineProps({
     promotion: {
@@ -16,36 +17,47 @@ defineEmits(['send', 'edit', 'delete']);
 <template>
     <div class="border-b pb-4 bg-neutral-light/60 rounded-lg shadow-lg p-4 relative">
         <div class="absolute inset-x-0 -top-2 flex justify-center">
-            <span :class="`bg-${promotion.status_color} text-white text-xs font-medium px-2.5 py-0.5 rounded-full`">{{ promotion.status }}</span>
+            <span :class="`bg-${promotion.status_color} text-white text-xs font-medium px-2.5 py-0.5 rounded-full`">{{
+                promotion.status }}</span>
         </div>
         <div class="mb-2 flex justify-between border-b border-neutral-dark pb-2">
             <div class="flex space-x-4">
-                <button @click="$emit('send', promotion)"
-                    class="text-primary hover:underline hover:-translate-y-0.5 transition ease-in-out duration-150 cursor-pointer">
-                    <PaperAirplaneIcon class="w-5 h-5 " />
+                <Tooltip text="Pošlji" location="top">
+                    <button @click="$emit('send', promotion)"
+                        class="text-primary hover:underline hover:-translate-y-0.5 transition ease-in-out duration-150 cursor-pointer">
+                        <PaperAirplaneIcon class="w-5 h-5 " />
 
-                </button>
-                <button @click="$emit('edit', promotion)" class="text-primary hover:underline hover:-translate-y-0.5 transition ease-in-out duration-150 cursor-pointer">
-                    <PencileSquareIcon class="w-5 h-5 " />
+                    </button>
+                </Tooltip>
 
-                </button>
+                <Tooltip text="Uredi" location="top">
+                    <button @click="$emit('edit', promotion)"
+                        class="text-primary hover:underline hover:-translate-y-0.5 transition ease-in-out duration-150 cursor-pointer">
+                        <PencileSquareIcon class="w-5 h-5 " />
+
+                    </button>
+                </Tooltip>
             </div>
-            <button @click="$emit('delete', promotion)" class="text-red-500 hover:underline hover:-translate-y-0.5 transition ease-in-out duration-150 cursor-pointer">
-                <TrashIcon class="w-5 h-5 " />
-            </button>
+            <Tooltip text="Izbriši" location="top">
+                <button @click="$emit('delete', promotion)"
+                    class="text-red-500 hover:underline hover:-translate-y-0.5 transition ease-in-out duration-150 cursor-pointer">
+                    <TrashIcon class="w-5 h-5 " />
+                </button>
+            </Tooltip>
         </div>
 
         <h3 class="text-lg font-semibold text-neutral-dark">{{ promotion.name }}</h3>
 
-        <div id="description" class="text-neutral-dark bg-neutral-100 border-neutral rounded-lg p-2" v-html="promotion.description"></div>
+        <div id="description" class="text-neutral-dark bg-neutral-100 border-neutral rounded-lg p-2"
+            v-html="promotion.description"></div>
 
         <p class="text-neutral-500 text-sm flex justify-between">
-        <span>
-            Začetni datum:<br>{{ new Date(promotion.start_date).toLocaleDateString('sl-SI') }}
-        </span>
-        <span >
-            Končni datum:<br>{{ new Date(promotion.end_date).toLocaleDateString('sl-SI') }}
-        </span>
+            <span>
+                Začetni datum:<br>{{ new Date(promotion.start_date).toLocaleDateString('sl-SI') }}
+            </span>
+            <span>
+                Končni datum:<br>{{ new Date(promotion.end_date).toLocaleDateString('sl-SI') }}
+            </span>
         </p>
     </div>
 </template>
@@ -55,22 +67,27 @@ defineEmits(['send', 'edit', 'delete']);
     font-size: 1.25rem;
     font-weight: bold;
 }
+
 ::v-deep(#description h2) {
     font-size: 1.125rem;
     font-weight: bold;
 }
+
 ::v-deep(#description h3) {
     font-size: 1rem;
     font-weight: bold;
 }
+
 ::v-deep(#description h4) {
     font-size: 0.875rem;
     font-weight: bold;
 }
+
 ::v-deep(#description h5) {
     font-size: 0.75rem;
     font-weight: bold;
 }
+
 ::v-deep(#description h6) {
     font-size: 0.625rem;
     font-weight: bold;
