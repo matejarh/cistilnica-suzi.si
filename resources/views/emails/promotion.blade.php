065022a101d16de4ed901e945ba2721d3c124ad6
Matej Arh <matej.arh@gmail.com>
2025-04-15 13:53:56 +0200

    feat: improve promotion email layout and enhance description rendering

--- resources/views/emails/promotion.blade.php
+++ resources/views/emails/promotion.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.email')
 
-@section('title', 'Nova promocija: ' . $promotion->name)
+@section('title')
+    Nova promocija: {{ $promotion->name }}
+@endsection
 
 @section('header-title', 'Nova promocija')
 
@@ -9,7 +11,7 @@
     <p>Imamo novo promocijo za vas:</p>
 
     <h2>{{ $promotion->name }}</h2>
-    <p>{{ $promotion->description }}</p>
+    <p>{!! $promotion->description !!}</p>
 
     @if ($promotion->image_url)
         <img src="{{ $promotion->image_url }}" alt="{{ $promotion->name }}" style="max-width: 100%; height: auto;">
@@ -20,3 +22,9 @@
 @endsection
 
 @section('footer-text', 'Čistilnica Suzi - brezhibna čistoča za vaše perilo.')
+
+@section('footer')
+    <p style="margin: 0;">Če želite prenehati prejemati naše e-pošte, se lahko <a href="{{ $unsubscribeUrl }}">odjavite tukaj</a>.</p>
+    <p style="margin: 0;">Za več informacij o naših storitvah obiščite našo spletno stran: <a href="{{ route('public.home') }}">Čistilnica Suzi</a>.</p>
+    <p style="margin: 0;">Vas zanima več? <a href="{{ route('public.promotions.active') }}">Oglejte si naše trenutne promocije</a>.</p>
+@endsection
