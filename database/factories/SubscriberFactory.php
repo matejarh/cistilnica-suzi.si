2937a4f76e8d9ba2e6d88ddbdd1d25e02660c42d
Matej Arh <matej.arh@gmail.com>
2025-04-15 13:53:03 +0200

    feat: create SubscriberFactory for generating Subscriber model instances

---
+++ database/factories/SubscriberFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Subscriber>
+ */
+class SubscriberFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'email' => $this->faker->unique()->safeEmail(),
+            'is_subscribed' => true,
+            'ip_address' => $this->faker->ipv4(),
+            'user_agent' => $this->faker->userAgent(),
+        ];
+    }
+}
