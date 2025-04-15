d71f5da9607d790e0b5b4827b283cccfdf4f3238
Matej Arh <matej.arh@gmail.com>
2025-04-15 23:56:24 +0200

    feat: add undo and redo functionality with corresponding toolbar buttons in TipTapInput component

--- resources/js/Components/TipTapInput.vue
+++ resources/js/Components/TipTapInput.vue
@@ -104,12 +104,12 @@ const errorClasses = computed(() => {
 const buttonClasses = computed(() => {
     return [
         ' font-bold text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600',
-        props.isSmall ? 'text-xs md:text-sm p-1' : 'p-2 text-xs md:text-base'
+        props.isSmall ? 'text-xs md:text-sm p-1' : 'p-1 text-xs md:text-base'
     ]
 })
 const iconClasses = computed(() => {
     return [
-        props.isSmall ? 'h-3 w-3 md:w-4 md:h-4' : 'h-3 w-3 md:w-6 md:h-6'
+        props.isSmall ? 'h-3 w-3 md:w-4 md:h-4' : 'h-3 w-3 md:w-4 md:h-4'
     ]
 })
 
@@ -174,7 +174,21 @@ const toolbarButtons = computed(() => [
         icon: icons.ParagraphIcon,
         label: '',
         active: editor.value?.isActive('paragraph')
-    }, */
+        }, */
+    {
+        condition: true,
+        action: () => editor.value.chain().focus().undo().run(),
+        icon: icons.UndoIcon,
+        label: '',
+        active: editor.value?.isActive('undo')
+    },
+    {
+        condition: true,
+        action: () => editor.value.chain().focus().redo().run(),
+        icon: icons.RedoIcon,
+        label: '',
+        active: editor.value?.isActive('redo')
+    },
     {
         condition: true,
         action: () => editor.value.chain().focus().toggleBold().run(),
@@ -230,10 +244,84 @@ const toolbarButtons = computed(() => [
         icon: icons.AlignJustifyIcon,
         label: '',
         active: editor.value?.isActive({ textAlign: 'justify' })
-    }
+    },
+    {
+        condition: !props.isSmall && props.hasHeadings,
+        action: () => editor.value.chain().focus().unsetAllMarks().run(),
+        icon: icons.ClearFormattingIcon,
+        label: '',
+        active: editor.value?.isActive( 'unsetAllMarks' )
+    },
+    {
+        condition: !props.isSmall && props.hasHeadings,
+        action: () => editor.value.chain().focus().toggleBulletList().run(),
+        icon: icons.BulletListIcon,
+        label: '',
+        active: editor.value?.isActive('bulletList')
+    },
+    {
+        condition: !props.isSmall && props.hasHeadings,
+        action: () => editor.value.chain().focus().toggleOrderedList().run(),
+        icon: icons.NumberedListIcon,
+        label: '',
+        active: editor.value?.isActive('orderedList')
+    },
+
 ])
 
 onBeforeUnmount(() => {
   editor.value.destroy()
 })
 </script>
+
+
+<style>
+
+.prose {
+    color: var(--tw-prose-body);
+    --tw-prose-body: oklch(37.3% .034 259.733);
+    --tw-prose-headings: oklch(21% .034 264.665);
+    --tw-prose-lead: oklch(44.6% .03 256.802);
+    --tw-prose-links: oklch(21% .034 264.665);
+    --tw-prose-bold: oklch(21% .034 264.665);
+    --tw-prose-counters: oklch(55.1% .027 264.364);
+    --tw-prose-bullets: oklch(87.2% .01 258.338);
+    --tw-prose-hr: oklch(92.8% .006 264.531);
+    --tw-prose-quotes: oklch(21% .034 264.665);
+    --tw-prose-quote-borders: oklch(92.8% .006 264.531);
+    --tw-prose-captions: oklch(55.1% .027 264.364);
+    --tw-prose-kbd: oklch(21% .034 264.665);
+    --tw-prose-kbd-shadows: NaN NaN NaN;
+    --tw-prose-code: oklch(21% .034 264.665);
+    --tw-prose-pre-code: oklch(92.8% .006 264.531);
+    --tw-prose-pre-bg: oklch(27.8% .033 256.848);
+    --tw-prose-th-borders: oklch(87.2% .01 258.338);
+    --tw-prose-td-borders: oklch(92.8% .006 264.531);
+    --tw-prose-invert-body: oklch(87.2% .01 258.338);
+    --tw-prose-invert-headings: #fff;
+    --tw-prose-invert-lead: oklch(70.7% .022 261.325);
+    --tw-prose-invert-links: #fff;
+    --tw-prose-invert-bold: #fff;
+    --tw-prose-invert-counters: oklch(70.7% .022 261.325);
+    --tw-prose-invert-bullets: oklch(44.6% .03 256.802);
+    --tw-prose-invert-hr: oklch(37.3% .034 259.733);
+    --tw-prose-invert-quotes: oklch(96.7% .003 264.542);
+    --tw-prose-invert-quote-borders: oklch(37.3% .034 259.733);
+    --tw-prose-invert-captions: oklch(70.7% .022 261.325);
+    --tw-prose-invert-kbd: #fff;
+    --tw-prose-invert-kbd-shadows: 255 255 255;
+    --tw-prose-invert-code: #fff;
+    --tw-prose-invert-pre-code: oklch(87.2% .01 258.338);
+    --tw-prose-invert-pre-bg: #00000080;
+    --tw-prose-invert-th-borders: oklch(44.6% .03 256.802);
+    --tw-prose-invert-td-borders: oklch(37.3% .034 259.733);
+    max-width: 100% !important;
+    font-size: 1rem;
+    line-height: 1.25rem;
+    margin-top: 0;
+    margin-bottom: 0;
+    padding: 0;
+    font-family: inherit;
+
+}
+</style>
