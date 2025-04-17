a4d3f47aa64b256c7cc843a08af415cb731181fe
Matej Arh <matej.arh@gmail.com>
2025-04-18 01:02:00 +0200

    feat: add ActionButton component with tooltip and click event support

---
+++ resources/js/Components/ActionButton.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Tooltip from './Tooltip.vue';
+
+defineProps({
+    text: { type: String, required: true },
+    icon: { type: String, required: true },
+    class: { type: String, default: '' },
+})
+defineEmits(['click'])
+</script>
+
+<template>
+    <Tooltip :text="text" location="top">
+        <button @click="$emit('click')" :class="[
+            'hover:underline hover:-translate-y-0.5 transition ease-in-out duration-150 cursor-pointer',
+        ]">
+            <component :is="icon" class="w-5 h-5" />
+        </button>
+    </Tooltip>
+</template>
